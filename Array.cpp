f57dd49d6cd889ecf7863ced2a93d73b409aa155
lotophagos272 <lotophagoi@sogang.ac.kr>
2020-10-09 23:08:41 +0900

    initial commit

---
+++ Array.cpp
@@ -0,0 +1,45 @@
+#include "Array.h"
+
+Array::Array(int size){
+	if(size<0)cout<<"ERROR Array size must be greater than 0"<<endl;
+	else {
+		data = new int[size];
+		len=size;
+	}
+}
+
+Array::~Array(){
+}
+
+int Array::length(){
+	return len;
+}
+
+int& Array::operator[](int i){
+	static int tmp;
+	if (0<=i && i<len){	
+		return data[i];
+	}
+	else{
+		cout<<"Array bound error!"<<endl;
+		return tmp;
+	}
+}
+
+int Array::operator [](int i) const{
+	if(0<=i && i<len){
+		return data[i];
+	}
+	else{
+		cout<<"Array bound error!"<<endl;
+		return 0;
+	}
+}
+
+void Array::print(){
+	cout<<"[";
+	for(int j=0;j<len;j++){
+		cout<<" "<<data[j];
+	}
+	cout<<"]"<<endl;
+}
